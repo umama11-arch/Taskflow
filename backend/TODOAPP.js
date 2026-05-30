086b92e5bc4aa71bae95c3392368a3e8183fb6e6
Umama11-arch <umamaishaq11@gmail.com>
2026-05-30 17:39:26 +0500

    depressed now

--- backend/TODOAPP.js
+++ backend/TODOAPP.js
@@ -6,7 +6,7 @@ app.use(express.json())
 
 const cors = require("cors");
 app.use(cors({
-  origin: "https://https://taskflow-kc7g.vercel.app/" ,                                                                                                                                                                          
+  origin: "https://taskflow-kc7g.vercel.app/" ,                                                                                                                                                                          
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
